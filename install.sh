361acef40b3c6bdef73b7e998a03a7c154534cf4
ghoste9624 <jackspade44@outlook.com>
2025-05-11 18:34:11 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #1/bin/bash                                
 apt update && apt upgrade -y && apt install whois dnsutils traceroute -y && apt autoremove -y                                       
+cd ~/
+rm sob
 cd ~/SOB                                    
 cp sob.sh sob                              
 mv sob ~/                                   
